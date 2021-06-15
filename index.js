48af94f381a5f6665b2464cc6c26495d29bfa7d3
William Farnsworth <williamcoreyf@gmail.com>
2021-06-15 12:52:18 -0400

    implemented viewEmployees and addEmployee function

--- index.js
+++ index.js
@@ -38,8 +38,7 @@ const mainMenu = () => {
             switch(answer.mainMenu) {
 
                 case 'Add employee':
-                    console.log('blah');
-                    mainMenu();
+                    addEmployee();
                     break;
 
                 case 'Add department':
@@ -53,8 +52,7 @@ const mainMenu = () => {
                     break;
 
                 case 'View employees':
-                    console.log('blah');
-                    mainMenu();
+                    viewEmployees();
                     break;
 
                 case 'View departments':
@@ -77,5 +75,50 @@ const mainMenu = () => {
                     break;
             }
         });
+}
+
+const viewEmployees = () => {
+
+    connection.query("SELECT employeeLog.id, employeeLog.first_name, employeeLog.last_name, roleLog.title, roleLog.salary, departmentLog.name FROM employeeLog INNER JOIN roleLog ON employeeLog.role_id = roleLog.id INNER JOIN departmentLog ON roleLog.department_id = departmentLog.id", (err, res) => {
+            if (err) throw (err)
+            console.table(res)
+            mainMenu();
+        })
+};
+
+const addEmployee = () => {
+    inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'firstName',
+            message: 'First name of the employee?'
+        },
+        {
+            type: 'input',
+            name: 'lastName',
+            message: 'Last name of the employee?'
+        },
+        {
+            type: 'input',
+            name: 'roleId',
+            message: 'Employee role id?'
+        },
+        {
+            type: 'input',
+            name: 'managerId',
+            message: 'Employee manager id?'
+        },
+    ])
+    .then((answer) => {
+        connection.query('INSERT into employeeLog SET ?', {
+            first_name: answer.firstName,
+            last_name: answer.lastName,
+            role_id: answer.roleId,
+            manager_id: answer.managerId
+        })
+        console.log('Employee Added')
+        viewEmployees()
+    })
 }
 
